1c0db0b487397be9eabb6df704ea693dfd5e1c28
Tomas Martinez <tomi12mc@gmail.com>
2024-07-11 20:58:31 -0500

    se quita la condicion para la carga de archivos estaticos

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -130,6 +130,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = 'inicio'
 
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
