74e35949ef7504ff8904fc8b4ca4de7660380c46
Tomas Martinez <tomi12mc@gmail.com>
2024-07-11 19:48:25 -0500

    se cambia la ruta de los archivos estaticos

--- vg_project/settings.py
+++ vg_project/settings.py
@@ -131,5 +131,5 @@ LOGIN_REDIRECT_URL = 'inicio'
 
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'media')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
